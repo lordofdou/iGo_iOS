81e2dc050d0cf95f06f66f54086550d0471271ab
LXX <eastblue_lan@126.com>
2016-03-31 10:54:16 +0800

    Initial Commit

---
+++ iGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iGo
+//
+//  Created by 兰轩轩 on 16/3/31.
+//  Copyright © 2016年 Eastblue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iGo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iGo
+//
+//  Created by 兰轩轩 on 16/3/31.
+//  Copyright © 2016年 Eastblue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
